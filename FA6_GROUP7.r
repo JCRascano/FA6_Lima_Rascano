fb62c2a9b579cfa374ecf0352d4672bee94a0e9b
JCRascano <127363617+JCRascano@users.noreply.github.com>
2023-03-09 10:13:44 +0800

    Add files via upload

---
+++ FA6_GROUP7.r
@@ -0,0 +1,59 @@
+
+#Paul Carlos T. Lima I and Jan Celine Rascano
+#Formative Assessment No. 6
+
+# FOR NUMBER 1 & 2 (START)
+
+
+#1. Set the probability of success to p = 0.2
+
+#2. Generate 1000 random variables from the geometric distribution.
+
+#GEOMETRIC DISTRIBUTION
+# p = 0.2 success rate and simulated 1000 times
+GD <- rgeom(1000, 0.2)
+GEODIS <- GD + 1
+GEODIS
+
+# FOR NUMBER 1 & 2 (END)
+
+
+
+
+
+# FOR NUMBER 3 & 4 (START)
+
+#3. Calculate some basic statistics:
+#mean_x <- mean(x)
+#var_x <- var(x)
+#sd_x <- sd(x) 
+
+#4. Print the results for mean, variance, and standard deviation 
+
+#For the "mean"
+mean_x <- mean(GEODIS)
+round(mean_x, 2)
+
+#For the "variance"
+var_x <- var(GEODIS)
+round(var_x, 2)
+
+#For the "standard deviation"
+sd_x <- sd(GEODIS) 
+round(sd_x, 2)
+
+# FOR NUMBER 3 & 4 (END)
+
+
+
+
+
+# FOR NUMBER 5 (START)
+
+# 5.) Print the Histogram of the Results
+data_x <- c(GEODIS)
+hist(data_x, xlab = "Randomly Generated Values", main = "For Geometric Distribution",col = "forestgreen", breaks = 10)
+
+# FOR NUMBER 5 (END)
+
+
